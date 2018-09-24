6199d7af26ad03a4691b1b52bac21a73fd17a47a
August Sjögren <august.sjogren@gmail.com>
2018-09-24 21:09:41 +0200

    Initial commit with basic grid system

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public Material goalMat;
+
+    public GameObject cell;
+    private GameObject[,] cells;
+    private static GameObject goal;
+
+    public int rows;
+    public int cols;
+    public float cellSize = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        goal = new GameObject();
+
+        cells = new GameObject[rows, cols];
+
+        cell.transform.localScale = new Vector3(cellSize, 1.0f, cellSize);
+
+        float positionX = -cellSize * rows / 2;
+        float positionZ = -cellSize * cols / 2;
+
+        Vector3 currentPosition = new Vector3();
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                currentPosition.x = positionX;
+                currentPosition.z = positionZ;
+
+                GameObject newCell = Instantiate(cell);
+                newCell.transform.position = currentPosition;
+                newCell.gameObject.name = "Cell [" + r + "," + c + "]";
+                cells[r, c] = newCell;
+
+                positionZ += cellSize;
+            }
+
+            positionX += cellSize;
+            positionZ = -cellSize * cols / 2;
+        }
+
+        cells[5, 5].gameObject.GetComponent<Renderer>().material = goalMat;
+
+        goal = cells[5, 5];
+
+        cell.gameObject.SetActive(false);
+    }
+
+    public static Transform GetTarget()
+    {
+        return goal.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/WorkerMind.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerMind : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
+
+    public Transform target;
+
+    public float step = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPosition = transform.position;
+        targetPosition = new Vector3();
+    }
+
+    public void SetTarget(Transform t)
+    {
+        target = t;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GridManager.GetTarget();
+        targetPosition = target.position;
+        targetPosition.y = initialPosition.y;
+
+        // TODO: implement A* here
+
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        }
+    }
+}
